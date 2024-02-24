34cb865689ab4b1997bcf71558457eb7a6eb7c4b
Nguyen Tuan Anh <talama191@gmail.com>
2024-02-24 09:22:41 +0700

    reup 22/.2

---
+++ C# basic/MangVaHam.cs
@@ -0,0 +1,94 @@
+
+public static class MangVaHam
+{
+
+
+    public static int DemSoLanSoChanXuatHienTrongChuoi(int[] arr)
+    {
+        int dem = 0;
+        foreach (int i in arr)
+        {
+            if (i % 2 == 0)
+            {
+                dem++;
+            }
+        }
+        return dem;
+    }
+
+    public static void TinhTong(int[] arr1, int[] arr2)
+    {
+        int sum = 0;
+        foreach (int i in arr1)
+        {
+            sum += i;
+        }
+        Console.WriteLine(sum);
+    }
+
+    public static void BaiTap2002()
+    {
+        int[,] arr = { { 1, 2 }, { 3, 4 }, { 3, 4 } };
+
+        int sum = 0;
+        int max = int.MinValue;
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            for (int j = 0; j < arr.GetLength(1); j++)
+            {
+                sum += arr[i, j];
+                if (arr[i, j] > max)
+                {
+                    max = arr[i, j];
+                }
+            }
+        }
+        Console.WriteLine(sum);
+        Console.ReadKey();
+    }
+
+    public static void ThemPhanTuVaoMang()
+    {
+        int[] ar1 = { 1, 2, 3, 4, 5, 0, 0, 0, 0, 0 };
+        Console.WriteLine("Array");
+        foreach (int i in ar1)
+        {
+            Console.Write(i + ",");
+        }
+        Console.WriteLine();
+
+        Console.Write("Input the insert number: ");
+        int insert = Int32.Parse(Console.ReadLine());
+
+        Console.Write("Input the index of insertion: ");
+        int index = Int32.Parse(Console.ReadLine());
+
+        int[] ar2 = new int[10];
+        for (int i = 0; i < ar1.Length; i++)
+        {
+            if (i == index)
+            {
+                ar2[i] = insert;
+            }
+            else
+            {
+                if (i > index)
+                {
+                    ar2[i] = ar1[i - 1];
+                }
+                else
+                {
+                    ar2[i] = ar1[i];
+                }
+            }
+        }
+
+        Console.WriteLine("New array");
+        foreach (int i in ar2)
+        {
+            Console.Write(i + ",");
+        }
+        Console.WriteLine();
+        Console.ReadKey();
+    }
+}
--- C# basic/Program.cs
+++ C# basic/Program.cs
@@ -3,68 +3,42 @@ public class Program
 {
     static void Main(string[] args)
     {
-        int[] arr = { 1, 2, 3 };
-        int[,] arr2 = { { 2, 3 }, { 3, 4 }, { 3, 7 } };
-        int[,,] arr2Chieu = {
-                {  { 2, 3 },{ 2, 3 }},
-                {  { 2, 3 },{ 2, 3 }},
-                {  { 2, 3 },{ 2, 3 }}};
-        // int[,] arr3 = new int[3, 2];
-        int[,] arr3 = {
-            { 2, 3, 4, 6 },
-            { 3, 4, 6, 2 },
-            { 3, 7, 10, 2 } ,
-            { 3, 4, 6, 2 },
-            { 3, 7, 4, 2 }
-            };
-        Console.WriteLine(arr3.GetLength(1));
-        for (int i = 0; i < arr3.GetLength(0); i++)
+        int count = 1;
+        for (int i = 0; i < 4; i++)
         {
-
-        }
-    }
-
-    public void ThemPhanTuVaoMang()
-    {
-        int[] ar1 = { 1, 2, 3, 4, 5, 0, 0, 0, 0, 0 };
-        Console.WriteLine("Array");
-        foreach (int i in ar1)
-        {
-            Console.Write(i + ",");
-        }
-        Console.WriteLine();
-
-        Console.Write("Input the insert number: ");
-        int insert = Int32.Parse(Console.ReadLine());
-
-        Console.Write("Input the index of insertion: ");
-        int index = Int32.Parse(Console.ReadLine());
-
-        int[] ar2 = new int[10];
-        for (int i = 0; i < ar1.Length; i++)
-        {
-            if (i == index)
+            for (int j = 0; j < count; j++)
             {
-                ar2[i] = insert;
+                Console.SetCursorPosition(j, i);
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("O");
             }
-            else
-            {
-                if (i > index)
-                {
-                    ar2[i] = ar1[i - 1];
-                }
-                else
-                {
-                    ar2[i] = ar1[i];
-                }
-            }
+            count++;
         }
+        int x = 0;
+        int y = 3;
+        Console.SetCursorPosition(x, y);
 
-        Console.WriteLine("New array");
-        foreach (int i in ar2)
+        while (true)
         {
-            Console.Write(i + ",");
+            ConsoleKeyInfo keyInfo = Console.ReadKey();
+            if (keyInfo.Key == ConsoleKey.UpArrow && y > 0)
+            {
+                y--;
+            }
+            else if (keyInfo.Key == ConsoleKey.DownArrow)
+            {
+                y++;
+            }
+            else if (keyInfo.Key == ConsoleKey.LeftArrow && x > 0)
+            {
+                x--;
+            }
+            else if (keyInfo.Key == ConsoleKey.RightArrow)
+            {
+                x++;
+            }
+            Console.SetCursorPosition(x, y);
         }
-        Console.WriteLine();
     }
 }
